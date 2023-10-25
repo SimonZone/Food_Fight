2d76845f2a73d1859ab2cab893d3d201c39f5b5b
Simon <sb.simonzone@gmail.com>
2023-10-25 11:46:33 +0200

    Made Canteen map and spawnpoints with foods

---
+++ Assets/_Scripts/GameManagement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameManagement : MonoBehaviour
+{
+    public List<GameObject> Spawnpoints;
+
+    public List<GameObject> Projectiles;
+
+    public int rutineTime = 10;
+
+    public void Start()
+    {
+        StartCoroutine(SpawnCoroutine());
+        //Instantiate(Projectiles[1], Spawnpoints[1].transform.position, Quaternion.identity);
+        //Instantiate(Projectiles[2], Spawnpoints[2].transform.position, Quaternion.identity);
+        //Instantiate(Projectiles[3], Spawnpoints[3].transform.position, Quaternion.identity);
+        //Instantiate(Projectiles[4], Spawnpoints[4].transform.position, Quaternion.identity);
+    }
+
+    private GameObject RandomProjectile()
+    {
+        int randomProjectileIndex = Random.Range(0, Projectiles.Count);
+        return Projectiles[randomProjectileIndex];
+    }
+
+    private Vector3 RandomSpawnPoint()
+    {
+        int randomSoawnPointIndex = Random.Range(0,Spawnpoints.Count);
+        var randomOffset = Random.insideUnitCircle * 1.5F;
+
+        var randomSpawnPoint = Spawnpoints[randomSoawnPointIndex].transform.position + (Vector3) randomOffset;
+        return randomSpawnPoint;
+    }
+
+    IEnumerator SpawnCoroutine()
+    {
+        for (int i = 0; i < rutineTime; i++)
+        {
+            Instantiate(RandomProjectile(), RandomSpawnPoint(), Quaternion.identity);
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+
+}
