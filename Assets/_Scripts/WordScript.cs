a6285644b767c4122d54d1c45317cb02221a5b93
ROlsen97 <100783426+ROlsen97@users.noreply.github.com>
2023-11-01 10:42:26 +0100

    update script

--- Assets/_Scripts/WordScript.cs
+++ Assets/_Scripts/WordScript.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WordScript : MonoBehaviour
 {
+    public Text wordUnderProjectile;
+    public string[] wordBank;
+    public string currentWord;
+    public string remainingWord;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetCurrentWord();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void SetCurrentWord()
+    {
+        
+    }
+    public void SetRemainingWord()
+    {
+
     }
 }
